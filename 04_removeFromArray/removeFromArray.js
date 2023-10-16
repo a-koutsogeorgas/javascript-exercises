835fc61b6e9044f9e983320b2197e4c085c3a335
thorfinn <144268162+a-koutsogeorgas@users.noreply.github.com>
2023-10-16 19:20:38 -0400

    Complete & Pass 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...removeArray) {
+    for (let i =0; i<removeArray.length; i++){
+        inputArray = inputArray.filter((val)=>val!==removeArray[i]);
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
