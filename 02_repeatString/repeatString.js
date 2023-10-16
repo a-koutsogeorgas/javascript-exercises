5a433dc6089acace7ebeafff6ef8568c4308dd12
thorfinn <144268162+a-koutsogeorgas@users.noreply.github.com>
2023-10-16 17:32:33 -0400

    Complete & pass tests for 01_helloWorld & 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num<0) return 'ERROR';
+    let output ='';
+    while(num){
+        output += string;
+        num--;
+    }
+    return output;
 };
 
 // Do not edit below this line
