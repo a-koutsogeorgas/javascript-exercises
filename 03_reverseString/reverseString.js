ae5d7db9b001fdd142d5a454341688aa31077a37
thorfinn <144268162+a-koutsogeorgas@users.noreply.github.com>
2023-10-16 17:53:57 -0400

    Complete & Pass all tests 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let reversedString = '';
+    for(let i = inputString.length; i>=0; i--){
+        reversedString += inputString.charAt(i);
+    }
+    return reversedString;
 };
-
+//READ THE SOLUTION REALLY ELEGANT!
 // Do not edit below this line
 module.exports = reverseString;
