d8a4f4af365e0e770ad16b706089ee89f7eaca88
thorfinn <144268162+a-koutsogeorgas@users.noreply.github.com>
2023-10-16 20:21:55 -0400

    Complete & Pass 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    let sum=0;
+    if ((numOne < 0 || numTwo < 0) || typeof numOne !== "number" ||typeof numTwo !=="number"){
+        return "ERROR";
+    }else{
+        if (numOne>numTwo){
+            for (let i=numTwo; i<=numOne; i++){
+                sum += i; 
+            }   
+        }else if(numTwo>numOne){
+            for (let i=numOne; i<=numTwo; i++){
+                sum += i; 
+            }
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
