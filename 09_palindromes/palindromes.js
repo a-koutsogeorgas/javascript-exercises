35b28b3be90317f2a3b3f9c933f9508b33940cda
thorfinn <144268162+a-koutsogeorgas@users.noreply.github.com>
2023-10-30 19:52:46 -0400

    all tests passed & refactor for simplicity using non regex solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+    return a.replaceAll(" ","").toLowerCase().split('').filter((letter) => punctuation.indexOf(letter) === -1).join("") == a.replaceAll(" ","").toLowerCase().split('').filter((letter) => punctuation.indexOf(letter) === -1).reverse().join("");
 };
 
 // Do not edit below this line
