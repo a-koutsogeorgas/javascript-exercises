d72eebc2efb1a756ac2b1dcd449d36327e1e9851
thorfinn <144268162+a-koutsogeorgas@users.noreply.github.com>
2023-10-30 18:43:17 -0400

    add power & factorial all tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,12 +14,17 @@ const multiply = function() {
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factorial = 1;
+  while(a){
+    factorial = factorial * a;
+    a--;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
