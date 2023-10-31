d3d57668cabbe13309aec0491c9449e9d12f5763
thorfinn <144268162+a-koutsogeorgas@users.noreply.github.com>
2023-10-31 12:36:50 -0400

    add findTheOldest all tests passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,6 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach((a)=> a.yearOfDeath ? "": a.yearOfDeath = new Date().getFullYear());
+    return people.sort((a,b)=> (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)? -1:1 )[0];
 };
 
 // Do not edit below this line
