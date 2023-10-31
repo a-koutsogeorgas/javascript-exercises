5daebd0a9f1354d6eab5f52c693c43f83a104aec
thorfinn <144268162+a-koutsogeorgas@users.noreply.github.com>
2023-10-30 20:48:00 -0400

    all tests passed dp solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n<0) return "OOPS";
+    if (n===0) return 0;
+    
+    let f = [0,1];
+    for(let i = 1; i < Number(n); i++){
+        f.push(f[f.length-1]+f[f.length-2]);
+    }
+    return f[f.length-1];
 };
 
 // Do not edit below this line
